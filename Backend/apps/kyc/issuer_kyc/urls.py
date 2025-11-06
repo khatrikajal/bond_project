dfe5382d012b12592f0c175c9568c025ae86d9fd
surajmorecombat <surajmorecombat@gmail.com>
2025-11-06 14:30:51 +0530

    demat account create update edit delete api done

--- Backend/apps/kyc/issuer_kyc/urls.py
+++ Backend/apps/kyc/issuer_kyc/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.CompanyAddressView import ComapnyAdressAPIView
 from .views.CompanyInformationCreateView import CompanyInformationCreateView,PanExtractionView,CompanyInfoGetView,CompanyInformationUpdateView,CompanyInfoDeleteView
+from .views.DemateAccountView import DematAccountCreateView,DematAccountGetView,DematAccountUpdateView,DematAccountDelateView
 
 app_name = 'issuer_kyc'
 
@@ -12,6 +13,10 @@ urlpatterns = [
     path('company-info/<uuid:company_id>/', CompanyInfoGetView.as_view(), name='company-info-get'),
     path('company-info/<uuid:company_id>/update/', CompanyInformationUpdateView.as_view(), name='company-info-update'),
     path("company-info/<uuid:company_id>/delete/", CompanyInfoDeleteView.as_view(), name="company-info-delete"),
+    path("company/<uuid:company_id>/demat/", DematAccountCreateView.as_view(), name="demat-account-create"),
+    path('company/demat/<int:demat_account_id>/get', DematAccountGetView.as_view(), name='demat-account-get'),
+    path("company/demat/<int:demat_account_id>/update", DematAccountUpdateView.as_view(), name="update-demat-account"),
+    path("company/demat/<int:demat_account_id>/delete", DematAccountDelateView.as_view(), name="delete-demat-account"),
    
   
 ]
