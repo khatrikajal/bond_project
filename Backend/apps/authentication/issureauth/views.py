69db0f252b9f8f9f777f1c4dd0e4df9fe02ff44e
khatrikajal <123454266+khatrikajal@users.noreply.github.com>
2025-11-10 18:14:36 +0530

    changes added

--- Backend/apps/authentication/issureauth/views.py
+++ Backend/apps/authentication/issureauth/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MobileOtpRequestSerializer,VerifyMobileOtpSerializer,EmailOtpRequestSerializer,VerifyEmailOtpSerializer,LoginRequestSerializer,VerifyLoginOtpSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 class SendMobileOtpView(APIView):
     permission_classes = [AllowAny]
     def post(self,request):
@@ -36,21 +38,55 @@ class VerifyMobileOtpView(APIView):
 
         if serializer.is_valid():
             data = serializer.save()
-            return Response({"status": "success", "message": data["message"], "data": data},
+            return Response({"status": "success", "message": data["message"], "data": {
+                "user_id":data["user_id"],
+                "mobile_number":data["mobile_number"],
+                "email":data["email"],
+                "email_verified":data["email_verified"],
+                "last_accessed_step":data["last_accessed_step"],
+                "company_information_id" : data["company_information_id"],
+                "access_token":data["access_token"],
+            }},
                 status=status.HTTP_200_OK,)
         
         return Response({"status": "error", "errors": serializer.errors},
             status=status.HTTP_400_BAD_REQUEST,)
     
-class SendEmailOtpView(APIView):
-    permission_classes = [AllowAny]
+# class SendEmailOtpView(APIView):
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes = [IsAuthenticated]
 
-    def post(self,request):
-        serializer = EmailOtpRequestSerializer(data=request.data)
+#     def post(self,request):
+#         serializer = EmailOtpRequestSerializer(data=request.data)
+#         if serializer.is_valid():
+#             data = serializer.save()
+
+#             return  Response(
+#                 {
+#                     "status": "success",
+#                     "message": data["message"],
+#                     "data": {
+#                         "user_id": data["user_id"],
+#                         "email": data["email"],
+#                         "otp_id": data["otp_id"],
+#                     },
+#                 },
+#                 status=status.HTTP_201_CREATED,
+#             )
+#         return Response(
+#             {"status": "error", "errors": serializer.errors},
+#             status=status.HTTP_400_BAD_REQUEST,
+#         )
+
+class SendEmailOtpView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = EmailOtpRequestSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
             data = serializer.save()
-
-            return  Response(
+            return Response(
                 {
                     "status": "success",
                     "message": data["message"],
@@ -69,33 +105,65 @@ class SendEmailOtpView(APIView):
     
 
 class VerifyEmailOtpView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
-    permission_classes =[AllowAny]
-
-    def post(self,request):
-        serializer = VerifyEmailOtpSerializer(data=request.data)
+    def post(self, request):
+        serializer = VerifyEmailOtpSerializer(data=request.data, context={"request": request})
 
         if serializer.is_valid():
             data = serializer.save()
-
             return Response(
                 {
                     "status": "success",
                     "message": data["message"],
+                    "last_accessed_step":0,
                     "data": {
                         "user_id": data["user_id"],
                         "email": data["email"],
                         "email_verified": data["email_verified"],
-                        "kyc_status":data['kyc_status'],
-                        "access_token": data['access_token']
-                    }
+                        "mobile_number": data["mobile_number"],
+                        # "kyc_status": data["kyc_status"],
+                        # "access_token": data["access_token"],
+                    },
                 },
-                status=status.HTTP_200_OK
+                status=status.HTTP_200_OK,
             )
+
         return Response(
             {"status": "error", "errors": serializer.errors},
-            status=status.HTTP_400_BAD_REQUEST
+            status=status.HTTP_400_BAD_REQUEST,
         )
+# class VerifyEmailOtpView(APIView):
+
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self,request):
+#         serializer = VerifyEmailOtpSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             data = serializer.save()
+
+#             return Response(
+#                 {
+#                     "status": "success",
+#                     "message": data["message"],
+#                     "data": {
+#                         "user_id": data["user_id"],
+#                         "email": data["email"],
+#                         "email_verified": data["email_verified"],
+#                         "mobile_number":data["mobile_number"],
+#                         "kyc_status":data['kyc_status'],
+#                         "access_token": data['access_token']
+#                     }
+#                 },
+#                 status=status.HTTP_200_OK
+#             )
+#         return Response(
+#             {"status": "error", "errors": serializer.errors},
+#             status=status.HTTP_400_BAD_REQUEST
+#         )
     
 class LoginRequestView(APIView):
     permission_classes = [AllowAny]
