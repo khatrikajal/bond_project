592cfeed2f66aff738accefcbd562e6b18369713
khatrikajal <123454266+khatrikajal@users.noreply.github.com>
2025-11-11 12:04:33 +0530

    bond_estimate added

---
+++ Backend/apps/bond_estimate/admin/__init__.py
@@ -0,0 +1,10 @@
+"""
+Admin package for bond_estimate app.
+You can split admin registrations into multiple files here.
+"""
+from django.contrib import admin
+# Example placeholder
+# from apps.bond_estimate.model.bond import Bond
+# @admin.register(Bond)
+# class BondAdmin(admin.ModelAdmin):
+#     pass
