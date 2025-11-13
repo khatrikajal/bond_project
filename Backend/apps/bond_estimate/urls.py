3ae94a0968955a7678964bfbb42f04d24aec35dc
khatrikajal <123454266+khatrikajal@users.noreply.github.com>
2025-11-13 17:37:56 +0530

    Implement code changes to enhance functionality and improve performance

--- Backend/apps/bond_estimate/urls.py
+++ Backend/apps/bond_estimate/urls.py
@@ -19,7 +19,14 @@ capital_detail = CapitalDetailsViewSet.as_view({
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views.borrowing_views import BorrowingDetailsViewSet  # adjust import if needed
+from .views.borrowing_views import BorrowingDetailsViewSet  
+from .views.CreditRatingView import (
+    CreditRatingCreateView,
+    CreditRatingListView,
+    CreditRatingDetailView,
+    CreditRatingBulkDeleteView,
+    CreditRatingAgencyChoicesView,
+)
 
 # Initialize the DRF router
 router = DefaultRouter()
@@ -53,6 +60,19 @@ urlpatterns = [
 
      #---------  RatingDetails ------------
      path("company/<uuid:company_id>/credit-rating/", CreditRatingCreateView.as_view(), name="credit-rating-create"),
+    
 
+    path('choices/', CreditRatingAgencyChoicesView.as_view(), name='credit-rating-choices'),
+    
+    # CRUD operations
+    path('ratings/<uuid:company_id>', CreditRatingListView.as_view(), name='credit-rating-list'),
+    path('ratings/create/<uuid:company_id>', CreditRatingCreateView.as_view(), name='credit-rating-create'),
+    path('ratings/<int:credit_rating_id>/<uuid:company_id>', CreditRatingDetailView.as_view(), name='credit-rating-detail'),
+    
+    # Bulk operations
+    path('ratings/bulk-delete/<uuid:company_id>', CreditRatingBulkDeleteView.as_view(), name='credit-rating-bulk-delete'),
+
+
+   
 ]
 
